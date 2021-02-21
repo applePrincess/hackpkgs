b1a2183e583b8416e5fc480f332b31e3cae493a6
Lein Matsumaru <appleprincess@appleprincess.io>
2021-02-21 07:17:01 +0000

    Add SSL package dependency

--- pkgs/tools/security/nikto/default.nix
+++ pkgs/tools/security/nikto/default.nix
@@ -1,6 +1,9 @@
-{ stdenv, fetchFromGitHub }:
-
-stdenv.mkDerivation rec {
+{ stdenvNoCC, fetchFromGitHub, perl }:
+let perlenv = perl.withPackages (pp: with pp; [
+      NetSSLeay
+    ]);
+in
+stdenvNoCC.mkDerivation rec {
   name = "nikto";
   src = fetchFromGitHub {
     owner = "sullo";
@@ -9,8 +12,18 @@ stdenv.mkDerivation rec {
     sha256 = "1q0ldwdcrz4xc5r88sharyxm8h5y7dxk61zkiwd5p056n5x5lm6d";
   };
 
+  buildInputs = [ perlenv ];
+
+  output = ["bin" "doc" "devDoc" "man" ];
+
   installPhase = ''
     mkdir -p $out/bin
+    mkdir -p $out/doc
+    mkdir -p $out/devDoc
+    mkdir -p $out/man/man1
     cp -r program/* $out/bin
+    cp -r documentation/* $out/doc
+    cp -r devdocs/* $out/devDoc
+    cp documentation/nikto.1 $out/man/man1/
   '';
 }
