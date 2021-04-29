c63352dfac4e956aa56ca897a04b806cccbd07db
Lein Matsumaru <appleprincess@appleprincess.io>
2021-04-29 06:17:32 +0000

    Copy packages so that you can use flake directly.

--- flake.nix
+++ flake.nix
@@ -6,9 +6,25 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import <nixpkgs> { system = system; };
+      let pkgs = import nixpkgs { system = system; };
       in rec {
-        dirbuster = pkgs.callPackage ./pkgs/tools/security/dirbuster { };
-        exploitdb = pkgs.callPackage ./pkgs/tools/security/exploitdb { };
-      });
+        packages = flake-utils.lib.flattenTree {
+          dirbuster = pkgs.callPackage ./pkgs/tools/security/dirbuster { };
+
+          exploitdb = pkgs.callPackage ./pkgs/tools/security/exploitdb { };
+
+          GTFOBLookup = pkgs.callPackage ./pkgs/tools/security/GTFOBLookup { };
+
+          msodbcsql = pkgs.callPackage ./pkgs/development/libraries/msodbcsql { };
+
+          mssql-tools = pkgs.callPackage ./pkgs/applications/misc/mssql-tools { };
+
+          nikto = pkgs.callPackage ./pkgs/tools/security/nikto { };
+
+          # pwncat = pkgs.pythonPackages.callPackage ./pkgs/tools/security/pwncat {} ;
+
+          stegsolve = pkgs.callPackage ./pkgs/tools/security/stegsolve { };
+        };
+      }
+    );
 }
